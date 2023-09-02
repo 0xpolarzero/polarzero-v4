71d317ef9585a17f480122c4ad556a225fa29426
polarzero <0xpolarzero@gmail.com>
2023-09-02 22:57:46 +0200

    Add portfolio page

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+export default function PortfolioPage() {
+  return (
+    <section className="container mx-auto flex flex-col space-y-2 p-4">
+      <Tabs defaultValue="audits" className="w-full">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="audits">Audits</TabsTrigger>
+          <TabsTrigger value="development">
+            Smart contracts development
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="audits">ok</TabsContent>
+        <TabsContent value="development">ok</TabsContent>
+      </Tabs>
+    </section>
+  )
+}
---
+++ types/portfolio.ts
@@ -0,0 +1,9 @@
+export type PortfolioItem = {
+  // type: "portfolio"
+  // id: string
+  // title: string
+  // subtitle: string | null
+  // description: string
+  // link: string
+  // pubDate: Date
+}
