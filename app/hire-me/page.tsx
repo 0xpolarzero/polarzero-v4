7e0cb898a9442895e94b4d14a455d93655e6cfad
polarzero <0xpolarzero@gmail.com>
2023-09-13 09:12:35 +0200

    Update hire-me text

--- app/hire-me/page.tsx
+++ app/hire-me/page.tsx
@@ -1,21 +1,46 @@
 "use client"
 
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
 import { ContactForm } from "@/components/contact-form"
 
 export default function HireMePage() {
   const handleSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {}
   return (
     <section className="container mx-auto flex flex-col space-y-4 p-4 px-8">
-      <div>
+      <div className="flex flex-col space-y-4">
         <p className="max-w-[900px] text-justify text-base">
-          If you would like to integrate smart contract with your project,{" "}
-          <span className="line-through">or if you need a security audit</span>,
-          reach out to me for a quote.
+          If you would like to integrate smart contracts into your project,{" "}
+          <span className="line-through">or if you need a security review</span>
+          , reach out to me for a quote.
         </p>
         <p className="max-w-[900px] text-justify text-muted-foreground">
-          Please give me as much information as possible about your project
-          up-front (concept, timeline, budget, scope, etc.). Feel free to attach
-          links to any relevant documents or resources.
+          Kindly share as much information as possible about your project
+          upfront. This includes details such as the concept, timeline, budget,
+          scope, and any other relevant information. Feel free to attach any
+          pertinent documents or resources.
+        </p>
+        <p className="max-w-[900px] text-justify text-muted-foreground">
+          For anything else, you can reach me at{" "}
+          <Link
+            className="underline"
+            href={siteConfig.links.email}
+            target="_blank"
+            rel="noreferer"
+          >
+            {siteConfig.links.email.replace("mailto:", "")}
+          </Link>{" "}
+          or on{" "}
+          <Link
+            className="underline"
+            href={siteConfig.links.twitter}
+            target="_blank"
+            rel="noreferer"
+          >
+            Twitter
+          </Link>
+          .
         </p>
       </div>
 
