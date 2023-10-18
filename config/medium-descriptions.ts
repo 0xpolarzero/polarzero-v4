e666fc3a2bdbb8b115f7a51a76ee4c23e003f8b2
polarzero <0xpolarzero@gmail.com>
2023-10-18 09:22:06 +0200

    Add article on delegation & indifference

--- config/medium-descriptions.ts
+++ config/medium-descriptions.ts
@@ -1,5 +1,7 @@
 export const mediumDescriptions: Record<string, string> = {
   // guid -> description
+  "https://medium.com/p/8c19d7167778":
+    "On delegation in modern democracies: how it breeds indifference, especially as it involves people delegating their understanding and awareness along with their power/authority; how decentralized systems can help break this cycle, by incentivizing active participation in governance.",
   "https://medium.com/p/725d7a6881cb":
     "A reflection on Chainlink’s latest milestones, and key aspects from a developer’s perspective.",
   "https://medium.com/p/99b9203c9824":
